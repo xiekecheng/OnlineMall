ef08157c3b67413489ca1b1d72da147aac4bffcc
xiekecheng <kc.xie@qq.com>
2021-03-25 16:10:12 +0800

    广告content，canal，redis
    广告content微服务，使redis与mysql数据同步

--- changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/CanalApplication.java
+++ changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/CanalApplication.java
@@ -1,16 +1,23 @@
 package com.changgou;
 
+import com.xpand.starter.canal.annotation.EnableCanalClient;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /****
  * @Description:
  * @Author: xiekecheng
  * @Date: 2021/03/24 20:48
  ***/
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 @EnableCanalClient
+@EnableFeignClients(basePackages = {"com.changgou.content.feign"})
 public class CanalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class,args);
+    }
 }
---
+++ changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/canal/CanalDataEventListener.java
@@ -0,0 +1,49 @@
+package com.changgou.canal;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.changgou.content.feign.ContentFeign;
+import com.changgou.content.pojo.Content;
+import com.xpand.starter.canal.annotation.*;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
+
+/****
+ * @Description:
+ * @Author: xiekecheng
+ * @Date: 2021/03/24 21:25
+ ***/
+@CanalEventListener
+public class CanalDataEventListener  {
+    /***
+     * 增加数据监听
+     * @param eventType
+     * @param rowData
+     */
+    @InsertListenPoint
+    public void onEventInsert(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        rowData.getAfterColumnsList().forEach((c) -> System.out.println("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
+    }
+
+    /***
+     * 修改数据监听
+     * @param rowData
+     */
+    @UpdateListenPoint
+    public void onEventUpdate(CanalEntry.RowData rowData) {
+        System.out.println("UpdateListenPoint");
+        rowData.getAfterColumnsList().forEach((c) -> System.out.println("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
+    }
+
+    /***
+     * 删除数据监听
+     * @param eventType
+     */
+    @DeleteListenPoint
+    public void onEventDelete(CanalEntry.EventType eventType) {
+        System.out.println("DeleteListenPoint");
+    }
+}
